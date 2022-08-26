a21d1ceebcf968d34d1a9989ee80dca5a8df9b86
Enis Gjini <gjinienis148@gmail.com>
2022-08-26 07:52:33 +0200

    Remove folder MyProducts.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import "./App.css";
-import MyProducts from "./components/MyProducts";
 
 function App() {
-  return (
-    <div className="App">
-      <MyProducts
-        emri_i_produktit={"Molla"}
-        pershkrimi={"E shijshme"}
-        qmimi={0.5}
-      />
-      <hr />
-      <MyProducts
-        emri_i_produktit={"Dardha"}
-        pershkrimi={"E shijshme"}
-        qmimi={0.7}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
